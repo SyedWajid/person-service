72e20ca3cd6236db3586686b9415ab823945e5b3
Syed Wajid <swajid@atex.com>
2020-10-27 22:34:55 +0500

    Initial commit

---
+++ src/test/java/com/mic3/personservice/PersonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mic3.personservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
